9306856b27789c5353f7d34e7cbf58251f4e07ba
Jason Suarez <swarecito@gmail.com>
2024-06-26 20:26:18 +0200

    feat: Add ReactFlow component with dynamic node and edge changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
 "use client";
-
-import ReactFlow, { Controls, Background } from "reactflow";
+import { useState, useCallback } from "react";
+import ReactFlow, {
+  Controls,
+  Background,
+  applyEdgeChanges,
+  applyNodeChanges,
+  type NodeChange,
+  type EdgeChange,
+  type Edge,
+} from "reactflow";
 import "reactflow/dist/style.css";
-const edges = [
+const initialEdges: Edge[] = [
   { id: "1-2", source: "1", target: "2", animated: true, label: "To the" },
   { id: "1-3", source: "1", target: "3", animated: false, label: "And" },
 ];
-const nodes = [
+const initialNodes = [
   {
     id: "1",
     type: "input",
@@ -25,9 +33,22 @@ const nodes = [
   },
 ];
 function Flow() {
+  const [nodes, setNodes] = useState(initialNodes);
+  const [edges, setEdges] = useState(initialEdges);
+  const onNodesChange = useCallback(
+    (changes: NodeChange[]) =>
+      setNodes((nds) => applyNodeChanges(changes, nds)),
+    []
+  );
+  const onEdgesChange = useCallback(
+    (changes: EdgeChange[]) =>
+      setEdges((eds) => applyEdgeChanges(changes, eds)),
+    []
+  );
+
   return (
     <div style={{ height: "100%", width: "100%" }}>
-      <ReactFlow nodes={nodes} edges={edges}>
+      <ReactFlow nodes={nodes} onNodesChange={onNodesChange} edges={edges}>
         <Background />
         <Controls />
       </ReactFlow>
