abc8863d702380f2b2abf8c519a70365d66c1f50
Jason Suarez <swarecito@gmail.com>
2024-06-26 20:10:37 +0200

    adding basic nodes and edges

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,32 @@
 
 import ReactFlow, { Controls, Background } from "reactflow";
 import "reactflow/dist/style.css";
-
+const edges = [
+  { id: "1-2", source: "1", target: "2", animated: true, label: "To the" },
+  { id: "1-3", source: "1", target: "3", animated: false, label: "And" },
+];
+const nodes = [
+  {
+    id: "1",
+    type: "input",
+    data: { label: "Welcome" },
+    position: { x: 250, y: 25 },
+  },
+  {
+    id: "2",
+    data: { label: "Jungle" },
+    position: { x: 100, y: 125 },
+  },
+  {
+    id: "3",
+    data: { label: "Back" },
+    position: { x: 400, y: 125 },
+  },
+];
 function Flow() {
   return (
     <div style={{ height: "100%", width: "100%" }}>
-      <ReactFlow>
+      <ReactFlow nodes={nodes} edges={edges}>
         <Background />
         <Controls />
       </ReactFlow>
