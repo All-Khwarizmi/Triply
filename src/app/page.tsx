647c8c99088c36790be7d2e1bf689763754e120b
Jason Suarez <swarecito@gmail.com>
2024-06-26 17:49:04 +0200

    Add configuration files and global styles

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import ReactFlow, { Controls, Background } from "reactflow";
+import "reactflow/dist/style.css";
+
+function Flow() {
+  return (
+    <div style={{ height: "100%", width: "100%" }}>
+      <ReactFlow>
+        <Background />
+        <Controls />
+      </ReactFlow>
+    </div>
+  );
+}
+
+export default Flow;
