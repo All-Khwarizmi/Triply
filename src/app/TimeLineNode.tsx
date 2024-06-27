e219a5b5c4c5f7ac64be5350687a4c44702d2fc8
Jason Suarez <swarecito@gmail.com>
2024-06-27 12:57:44 +0200

    feat: Implement click event handling for TimeLineNode component

--- src/app/TimeLineNode.tsx
+++ src/app/TimeLineNode.tsx
@@ -2,16 +2,42 @@ import React from "react";
 import type { NodeProps } from "reactflow";
 
 function TimeLineNode(props: NodeProps) {
+  const timeLineRef = React.useRef<HTMLDivElement>(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (timeLineRef.current) {
+      const rect = timeLineRef.current.getBoundingClientRect();
+      const clickX = event.clientX - rect.left;
+      const clickY = event.clientY - rect.top;
+      console.log("Clicked at:", clickX, clickY);
+
+      // Calculate the point to insert the node based on clickX
+      const totalWidth = rect.width;
+      const positionPercentage = (clickX / totalWidth) * 100;
+      console.log("Position Percentage:", positionPercentage);
+
+      // Here you can insert the logic to add a node at this position
+    }
+  };
+
   return (
     <>
       <div
+        
+      />
+      {/* biome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
+      <div
+        ref={timeLineRef}
+        onClick={handleClick}
         className={`
-      nodrag text-black
+        nodrag text-black
         border rounded-md
         flex flex-col p-1
         w-screen border-white bg-gray-300
       `}
-      />
+      >
+        {/* Existing content of the node */}
+      </div>
     </>
   );
 }
