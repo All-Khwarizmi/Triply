b7d287f84a31bafe3023518f34effeeab973fe70
Jason Suarez <swarecito@gmail.com>
2024-07-01 21:52:39 +0200

    refactor: Adjust position of start and end nodes in create-node-helpers.ts

--- src/app/timectx/hooks/useHandleNodeHooks.ts
+++ src/app/timectx/hooks/useHandleNodeHooks.ts
@@ -48,8 +48,8 @@ const useHandleNodeHooks = () => {
     position: { x: number; y: number }
   ) {
     // Implement the logic to update the position of the node
-    setNodes((nds) =>
-      nds.map((node) => {
+    setNodes((nds) => {
+      const newNodes = nds.map((node) => {
         if (node.id === nodeId) {
           return {
             ...node,
@@ -60,8 +60,31 @@ const useHandleNodeHooks = () => {
           };
         }
         return node;
-      })
-    );
+      });
+
+      return newNodes;
+    });
+  }
+  // Should take a new node and insert it into the nodes array. Should check the date of the node and insert it in the correct position. Also should update the edges array to reflect the new node. Also should take care of the position of the new node so that it is placed in the correct position regarding the other nodes.
+  function addNode(node: Node) {
+    if (nodes.length < 2) {
+      throw new Error(
+        "The timeline should have at least two nodes to add a new node."
+      );
+    }
+    // Check the position of the last node before the new one (by date). We take the index for reference.
+    const lastNodeIndex = nodes.findIndex((n) => n.data.date > node.data.date);
+    // If the last node is the start node, we insert the new node after it.
+    if (lastNodeIndex === -1) {
+      throw new Error(
+        "The new node date is not set properly or there are not nodes in the timeline."
+      );
+    }
+    if (lastNodeIndex === 0) {
+      throw new Error(
+        "The new node date is not set properly or there are not nodes in the timeline."
+      );
+    }
   }
   return {
     nodes,
