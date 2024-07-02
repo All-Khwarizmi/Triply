643eae50d073fccf5a3bc25520f17f4b7a24371a
Jason Suarez <swarecito@gmail.com>
2024-07-02 18:02:06 +0200

    feat: Add PanelMenu component for top-right panel with zoom and center buttons

---
+++ src/app/timectx/Panel.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { useReactFlow, Panel } from "reactflow";
+function PanelMenu() {
+  const { zoomIn, zoomOut, setCenter, fitView } = useReactFlow();
+  return (
+    <Panel
+      position="top-right"
+      className="flex gap-4  p-4  rounded-lg shadow-md"
+    >
+      {/* biome-ignore lint/a11y/useButtonType: <explanation> */}
+      <button onClick={() => zoomIn({ duration: 800 })}>zoom in</button>
+      {/* biome-ignore lint/a11y/useButtonType: <explanation> */}
+      <button onClick={() => zoomOut({ duration: 800 })}>zoom out</button>
+      {/* biome-ignore lint/a11y/useButtonType: <explanation> */}
+      <button onClick={() => fitView()}>center</button>
+    </Panel>
+  );
+}
+
+export default PanelMenu;
