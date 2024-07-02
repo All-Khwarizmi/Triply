78b4f8d904750c62ca3133eb1d16576c0d050c01
Jason Suarez <swarecito@gmail.com>
2024-07-03 01:00:48 +0200

    refactor: Add NodeDataSchema and NodeSchema to handle node data and structure

---
+++ test/schemas.spec.ts
@@ -0,0 +1,50 @@
+import dayjs from "dayjs";
+import { expect, test, describe, it } from "vitest";
+import { createEndNodeExtend } from "./node-extend-helper";
+import { NodeExtendSchema } from "../src/app/timectx/helpers/schemas";
+import type { NodeExtend } from "@/app/timectx/helpers/list";
+
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+export function isNodeExtend(node: any): node is NodeExtend {
+  const isValid = NodeExtendSchema.safeParse(node);
+  if (!isValid.success) {
+    return false;
+  }
+  if (node.data.nextNode && !isNodeExtend(node.data.nextNode)) {
+    return false;
+  }
+  if (node.data.prevNode && !isNodeExtend(node.data.prevNode)) {
+    return false;
+  }
+  return true;
+}
+
+describe("List schemas", () => {
+  test("should be able to parse a valid NodeExtend", () => {
+    const node = createEndNodeExtend({
+      id: "node-42",
+      startDate: dayjs(new Date()).add(3, "day"),
+      updateNodePosition: () => {},
+    });
+    const nodeTwo = createEndNodeExtend({
+      id: "node-43",
+      startDate: dayjs(new Date()).add(4, "day"),
+      updateNodePosition: () => {},
+    });
+    const fullNode: NodeExtend = {
+      ...node,
+      data: {
+        ...node.data,
+        nextNode: nodeTwo,
+        prevNode: node,
+      },
+    };
+    const isValid = NodeExtendSchema.safeParse(node);
+    if (!isValid.success) {
+      console.error(isValid.error.errors);
+    }
+    expect(isNodeExtend(node)).toBeTruthy();
+    expect(isNodeExtend(nodeTwo)).toBeTruthy();
+    expect(isNodeExtend(fullNode)).toBeTruthy();
+  });
+});
