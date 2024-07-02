3adeb4d88aef5859dbdbd0a27e3aa9ea982e900e
Jason Suarez <swarecito@gmail.com>
2024-07-02 18:01:56 +0200

    refactor: Add ReactFlowProvider to improve page rendering performance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 "use client";
 import "reactflow/dist/style.css";
 import App from "./timectx/page";
+import { ReactFlowProvider } from "reactflow";
 
 function Page() {
-  return <App />;
+  return (
+    <ReactFlowProvider>
+      <App />
+    </ReactFlowProvider>
+  );
 }
 
 export default Page;
