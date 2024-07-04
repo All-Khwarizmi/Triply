45de59a2d404f5b973c35d5088e88da7fbd69aae
Jason Suarez <swarecito@gmail.com>
2024-07-04 21:38:22 +0200

    feat: Add HeroFive component for landing page
    This commit adds the HeroFive component to the project, which is used in the landing page. The HeroFive component provides a visually appealing and attention-grabbing section for the landing page, enhancing the user experience and improving the overall design of the application.

---
+++ src/components/HeroFive.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import * as z from "zod";
+import Image from "next/image";
+import Link from "next/link";
+import Balancer from "react-wrap-balancer";
+import { ArrowRight } from "lucide-react";
+
+// UI Components
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+// Custom Components
+import { Section, Container } from "./hero-five-props";
+
+// Assets
+
+import Header from "./ui/Header";
+
+const formSchema = z.object({
+  email: z.string().email({
+    message: "Please enter a valid email address.",
+  }),
+});
+
+export default function Hero() {
+  return (
+    <Section>
+      <Container>
+        <div className="flex flex-col items-center text-center mb-6">
+          <Header />
+          <h1 className="!mb-0">
+            <Balancer>
+              <h3 className="text-4xl mb-6">
+                Plan Everything, Decide on the Fly
+              </h3>
+            </Balancer>
+          </h1>
+          <h3 className="text-muted-foreground">
+            <Balancer>
+              Start your adventure by choosing your trip dates.
+            </Balancer>
+          </h3>
+          <form onSubmit={() => {}} className="space-y-2 text-left">
+            <Input />
+            <Button className="w-full" type="submit">
+              Submit
+            </Button>
+          </form>
+          <div className="my-8 h-96 w-full overflow-hidden rounded-lg border md:h-[480px] md:rounded-xl">
+            <Image
+              className="not-prose h-full w-full object-cover object-bottom"
+              src={"/hero.jpg"}
+              width={1920}
+              height={1080}
+              alt="hero image"
+              placeholder="blur"
+              blurDataURL="/hero.jpg"
+            />
+          </div>
+        </div>
+      </Container>
+    </Section>
+  );
+}
