2971c4aec4e35a81f867c6837af6873b367505ab
Jason Suarez <swarecito@gmail.com>
2024-07-04 23:07:46 +0200

    refactor: Update TripEditor component and dependencies
    This commit refactors the TripEditor component by removing unnecessary code and updating the dependencies. The useHandleNodeHooks custom hook is also updated to accept the tripDates as an argument, allowing for more flexibility in setting the start and end dates of the node list. Additionally, the date-fns and react-date-range dependencies are added to enhance the functionality and user experience of the application.

--- src/components/TripEditor.tsx
+++ src/components/TripEditor.tsx
@@ -1,20 +1,15 @@
 "use client";
 import type React from "react";
-import ReactFlow, { Background, Controls, ReactFlowProvider } from "reactflow";
+import ReactFlow, { Background, Controls } from "reactflow";
 import "reactflow/dist/style.css";
-import "reactflow/dist/style.css";
-import DrawerMenu from "@/components/ui/DrawerMenu";
-import Header from "@/components/ui/Header";
 import CustomNode from "@/app/timectx/CustomNode";
 import PanelMenu from "@/app/timectx/Panel";
 import { StageColumnNode } from "@/app/timectx/StageColumn";
 import useHandleNodeHooks from "@/app/timectx/hooks/useHandleNodeHooks";
-import TimeLineNodeElement from "@/app/timeline/TimelineNodeElement";
 import type { Range } from "react-date-range";
 
 const nodeTypes = {
   customNode: CustomNode,
-  timeline: TimeLineNodeElement,
   column: StageColumnNode,
 };
 function TripEditor(props: { tripDates: Range }) {
@@ -28,7 +23,7 @@ function TripEditor(props: { tripDates: Range }) {
     updateNodeMetadata,
   } = useHandleNodeHooks(props);
   return (
-    <div className="h-full w-full flex flex-col ">
+    <div className="h-[70vh] w-full flex flex-col ">
       <ReactFlow
         nodes={nodes}
         edges={edges}
