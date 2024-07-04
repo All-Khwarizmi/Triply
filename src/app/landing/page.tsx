f75f1f9d880bd719028755dddf290646cf20f09c
Jason Suarez <swarecito@gmail.com>
2024-07-04 23:08:08 +0200

    refactor: Remove duplicate import of reactflow/dist/style.css in page.tsx

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import Hero from "@/components/HeroFive";
 import React from "react";
+import ReactFlowProviderWrapper from "../ReactFlowProviderWrapper";
 
 function page() {
   return <Hero />;
