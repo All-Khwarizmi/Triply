34f4a4434edb88c281ed140b338351d64b92454e
Jason Suarez <swarecito@gmail.com>
2024-07-05 18:10:53 +0200

    feat: Add AddChildNodePopover component for adding child nodes
    This commit adds the AddChildNodePopover component, which allows users to add child nodes to a parent node. The component includes input fields for specifying the trip name, trip date, and trip body of the child node. When the "Add" button is clicked, the component triggers the handleAddChild function to create and add the child node to the parent node. This change enhances the functionality and flexibility of the application by providing an intuitive way to manage the node hierarchy.

---
+++ src/components/AddChildNodePopover.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Button } from "./ui/button";
+import dayjs from "dayjs";
+import type { NodeExtend } from "@/app/timectx/helpers/list";
+import { createTripNodeExtend } from "../../test/node-extend-helper";
+
+function AddChildNodePopover(props: {
+  handleAddChild: ({ childNode }: { childNode: NodeExtend }) => void;
+}) {
+  const [tripDate, setTripDate] = useState<string>(
+    dayjs().format("YYYY-MM-DD")
+  );
+  const [tripName, setTripName] = useState<string>("");
+  const [tripBody, setTripBody] = useState<string>("");
+  function handleSubmission() {
+    const node: NodeExtend = createTripNodeExtend({
+      startDate: dayjs(tripDate),
+      name: tripName,
+      body: tripBody,
+    });
+    props.handleAddChild({ childNode: node });
+  }
+  return (
+    <Popover>
+      <PopoverTrigger>
+        <Button>Add Child Node</Button>
+      </PopoverTrigger>
+      <PopoverContent>
+        <div className="p-4 space-y-4">
+          <Label htmlFor="trip-name">Trip Name</Label>
+          <Input
+            id="trip-name"
+            value={tripName}
+            onChange={(e) => setTripName(e.target.value)}
+          />
+          <Label htmlFor="trip-date">Trip Date</Label>
+          <Input
+            id="trip-date"
+            type="date"
+            value={tripDate}
+            onChange={(e) => setTripDate(e.target.value)}
+          />
+          <Label htmlFor="trip-body">Trip Body</Label>
+          <Input
+            id="trip-body"
+            value={tripBody}
+            onChange={(e) => setTripBody(e.target.value)}
+          />
+          <Button onClick={handleSubmission}>Add</Button>
+        </div>
+      </PopoverContent>
+    </Popover>
+  );
+}
+
+export default AddChildNodePopover;
