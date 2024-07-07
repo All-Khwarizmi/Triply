5d866cecab0cd8960e1663173662db4e3e346a3e
Jason Suarez <swarecito@gmail.com>
2024-07-07 13:15:24 +0200

    feat: Add error handling to GET request in route.ts

--- src/app/api/trip/route.ts
+++ src/app/api/trip/route.ts
@@ -2,7 +2,17 @@ import { remoteDatabase } from "@/drizzle";
 
 export const dynamic = "force-dynamic"; // defaults to auto
 export async function GET(request: Request) {
-  return new Response("Hello world!", { status: 200 });
+  try {
+    const { searchParams } = new URL(request.url);
+    const id = searchParams.get("id");
+    if (!id) {
+      return new Response("Missing id", { status: 400 });
+    }
+    const item = await remoteDatabase.getItem(id);
+    return new Response(item, { status: 200 });
+  } catch (error) {
+    return new Response("Error parsing JSON", { status: 400 });
+  }
 }
 export async function POST(request: Request) {
   try {
