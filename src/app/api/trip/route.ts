1d4b6212436c43ecbf1c901a242bcb154a6159ab
Jason Suarez <swarecito@gmail.com>
2024-07-07 13:41:32 +0200

    refactor: Update setItem method in RemoteDatabase class
    Refactor setItem method in RemoteDatabase class to return a Trip object instead of an array of objects. This improves code accuracy and ease of use for subsequent logic.

--- src/app/api/trip/route.ts
+++ src/app/api/trip/route.ts
@@ -21,8 +21,8 @@ export async function POST(request: Request) {
   try {
     const body = await request.json();
     console.log(body);
-    const id = await remoteDatabase.setItem(body.key, body.value);
-    return new Response(JSON.stringify({ ...body, id }), { status: 200 });
+    const item = await remoteDatabase.setItem(body.key, body.value);
+    return new Response(JSON.stringify({ ...body, item }), { status: 200 });
   } catch (error) {
     return new Response("Error parsing JSON", { status: 400 });
   }
