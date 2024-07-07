ccc99d1a8a86b26cac6e213da4712c582c1c17e2
Jason Suarez <swarecito@gmail.com>
2024-07-07 14:29:31 +0200

    refactor: Update getItem method in get-item.ts to handle server-side rendering

--- src/actions/get-item.ts
+++ src/actions/get-item.ts
@@ -1,15 +1,23 @@
+"use server";
 import type { Trip } from "@/drizzle/schema";
 
-export async function getItem(key: string): Promise<Trip | null> {
+export async function getItem(key: string): Promise<{ data: Trip } | null> {
   try {
-    const response = await fetch(`/api/trip?id=${key}`);
+    const response = await fetch(`http://localhost:3000/api/trip?id=${key}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
     if (!response.ok) {
       console.error(response.statusText);
       return null;
     }
-    const body = await response.json();
-    return body as Trip;
+    const data = await response.json();
+    const body = JSON.parse(JSON.stringify({ data }));
+    return body as { data: Trip };
   } catch (error) {
+    console.error(error);
     return null;
   }
 }
